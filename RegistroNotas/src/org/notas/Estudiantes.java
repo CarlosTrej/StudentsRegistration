859321aea448dd2669d4a8c6baffa4e6e96cb0f3
Carlos Trejo <carlostrejo182@gmail.com>
2021-12-29 00:40:45 -0600

    add exercise.java

---
+++ RegistroNotas/src/org/notas/Estudiantes.java
@@ -0,0 +1,12 @@
+package org.notas;
+
+public class Estudiantes {
+	
+	private int nMatricula;
+	private String nombre;
+	private int nota1;
+	private int nota2;
+	private int nota3;
+	
+
+}//Estudiantes
